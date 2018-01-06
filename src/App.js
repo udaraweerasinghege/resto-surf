95f0a9172f2017d60a506d409525633ced3c6321
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2018-01-06 02:30:43 -0500

    initial stuffs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import RestoTile from './components/RestoTile/RestoTile'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RestoTile/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/RestoTile/RestoTile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RestoTile extends Component {
+  render() {
+    return (
+      <div className="tile-container">
+        <img src="hi" className="resto-img" alt="logo" />
+        <div className="resto-title">HIHIHI</div>
+      </div>
+    );
+  }
+}
+
+export default RestoTile;

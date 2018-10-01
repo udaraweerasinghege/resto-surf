e28ed190de935ce2dd4850adbcc0c09f0c4931c5
Meagan Pau <meagan.pau@crowdlinker.com>
2018-10-01 13:23:32 -0400

    remove unused props

--- src/components/pages/page-404.js
+++ src/components/pages/page-404.js
@@ -3,7 +3,7 @@ import Banner from "../partials/banner";
 import Layout from "../layout";
 import SearchBar from "../partials/searchBar";
 
-export default ({ restaurants }) => (
+export default () => (
     <Layout>
         <Banner title="404" subtitle="Sorry, couldn't find that page." image="/images/header-5.jpg" />
         <div className="container">
